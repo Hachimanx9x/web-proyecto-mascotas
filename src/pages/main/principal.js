d3bc46b2d6b3439981eafe08200074751de7fb59
isa <isagobaro@gmail.com>
2021-05-02 14:12:49 -0500

    actualización desde el proyecto local isa

--- src/pages/main/principal.js
+++ src/pages/main/principal.js
@@ -1,12 +1,26 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Principal from "../andres/uno";
+import Cuenta from "../isabella/Cuenta";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+      <Route path="/campeta">
+          <Cuenta />
+        </Route>
+      <Route path="/juan">
+          <Cuenta />
+        </Route>
+      <Route path="/diego">
+          <Cuenta />
+        </Route>
+      <Route path="/isabella">
+          <Cuenta />
+        </Route>
         <Route path="/">
           <Principal />
         </Route>
+     
       </Switch>
     </BrowserRouter>
   );
