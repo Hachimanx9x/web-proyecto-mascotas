7aa0fc664bdb242f5378dff9e45f78a65d1cd228
Hachimanx9x <diego_fer.bolanos@uao.edu.co>
2021-05-03 14:11:06 -0500

    footer

--- src/pages/diego/components/Carousel.js
+++ src/pages/diego/components/Carousel.js
@@ -4,7 +4,13 @@ export default function Carousel({ title, pets }) {
   return (
     <div className="o-container-carousel">
       <div className="o-min-Nav">{title}</div>
-      <Slider itemsToShow={5} pagination={false} showArrows={true}>
+      <Slider
+        itemsToShow={5}
+        pagination={false}
+        showArrows={true}
+        outerSpacing={0}
+        verticalMode={false}
+      >
         {pets.map((ele, i) => (
           <div>
             <Cards img={ele.img} />
--- src/pages/diego/components/Footer.js
+++ src/pages/diego/components/Footer.js
@@ -1,9 +1,40 @@
+import PhoneInTalkIcon from "@material-ui/icons/PhoneInTalk";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
 export default function Footer({ numers, email, redso }) {
   return (
     <div className="o-container-footer">
-      <div className="o-footer-numers"></div>
-      <div className="o-footer-email"></div>
-      <div className="o-footer-red"></div>
+      <div className="o-footer">
+        <div className="o-footer-numers">
+          <PhoneInTalkIcon
+            className="o-svg"
+            htmlColor="#FFFFFF"
+            fontSize="inherit"
+          />
+          <div className="o-footer-num">{numers}</div>
+        </div>
+        <div className="o-footer-email">
+          <MailOutlineIcon className="o-svg" />
+          {email}
+        </div>
+        <div className="o-footer-red">
+          {redso.map((ele, i) =>
+            ele.red === "Facebook" ? (
+              <a key={i} href={ele.url}>
+                <FacebookIcon className="o-svg" />
+              </a>
+            ) : ele.red === "Instagram" ? (
+              <a key={i} href={ele.url}>
+                <InstagramIcon className="o-svg" />
+              </a>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 }
